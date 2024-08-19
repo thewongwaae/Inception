4e6ef4fc2093adc311959e0b1f6e5102b9fe0370
Wong Hong You <wonghongyou@gmail.com>
2024-08-19 23:50:55 +0800

    Mariadb 3306 exposed

--- srcs/requirements/mariadb/conf/mdb-conf.sh
+++ srcs/requirements/mariadb/conf/mdb-conf.sh
@@ -11,4 +11,4 @@ mariadb -e "FLUSH PRIVILEGES;" # Flush privileges to apply changes
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
 # mysqld will start MariaDB in safe mode, and store files in '/var/lib/mysql' 
-mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql' # Restart in background
+mysqld_safe --port=3306 --bind-address=0.0.0.0 # Restart in background
--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -6,7 +6,7 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 cd /var/www/wordpress
-chmod -R 755 /var/www/wordpress
+chmod -R 777 /var/www/wordpress
 chown -R www-data:www-data /var/www/wordpress
 
 #----------check connection to mariadb----------#
